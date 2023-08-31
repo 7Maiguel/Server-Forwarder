d3d31f02531fdf376b2a5d89dc7e94175c72f2f2
“Lud <ludingnumpaque@gmail.com>
2023-08-31 17:31:24 -0500

    Changes for send response

--- WebSockets/app/Mod_Server_Forward.py
+++ WebSockets/app/Mod_Server_Forward.py
@@ -1,6 +1,7 @@
 from lib.Lib_WS import WebSocketFuseaccess
 from lib.Lib_Settings import Get_Socket_Timeout, Get_Forward_server
 from lib.Lib_Request_Json import send_petition
+from operator import attrgetter
 import time
 import os
 
@@ -13,8 +14,13 @@ server_to_forward = Get_Forward_server()
 class WSServerForward(WebSocketFuseaccess):
     def on_message(self, msg):
         message = msg["message"]
-        # send_petition()
-        print(message)
+        print(msg)
+        response = send_petition(
+            "http://192.168.0.4:4000/api/fuseaccess/"+message["url"], method=message["method"], json_data=message["data"], headers=message["headers"])
+        if response:
+            msg["response"]=response.json()
+            self.send_message("get_data",msg)
+            # super().send_message(response.json)
 
 
 ws = WSServerForward("ServerForwardChannel")
